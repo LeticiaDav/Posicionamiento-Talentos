dc7eed7a1ac63e28346acb95d642af485910404b
Genesis Jesus <genesisdev96@outlook.com>
2017-10-25 12:02:34 -0500

    Se creo la estructura del registro de usuarios
    Se creo la estructura basica para el registro de usuarios.

--- registro.php
+++ registro.php
@@ -1,23 +1,24 @@
 
 <!--
-  registro.php
-  https://github.com/Genesisj96Dev/Posicionamiento-Talentos
+#registro.php - 1.0.0
 
-  Copyright 2017 WORkTECS
+https://github.com/Genesisj96Dev/Posicionamiento-Talentos
 
-  Desarrollado por Genesis Jesus Reyes Osnaya
+Copyright 2017 WORkTECS
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
+Desarrollado por Genesis Jesus Reyes Osnaya
 
-      http://www.apache.org/licenses/LICENSE-2.0
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 -->
 
 <!DOCTYPE html>
@@ -25,10 +26,54 @@
   <head>
     <meta charset="utf-8">
     <title>Posicionamiento de Talentos | Registro de usuario</title>
-    <link rel="stylesheet" type="text/css" href="CSS/semantic.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min.css" integrity="sha256-wT6CFc7EKRuf7uyVfi+MQNHUzojuHN2pSw0YWFt2K5E=" crossorigin="anonymous" />
+    <link rel="stylesheet" type="text/css" href="css/semantic.css">
+    
+    <!-- DESCOMENTAR LA LINEA DE ABAJO CUANDO EL PROYECTO ESTE EN VERSION FINAL -->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min.css"/> -->
   </head>
   <body>
+    <div class="ui raised very padded center aligned text container segment">
+      <h1 class="ui header">Registro de usuario</h1>
+      <br>
+      <div class="field">
+        <div class="ui left icon input">
+          <i class="mail outline icon"></i>
+          <input type="nombre" name="nombre" placeholder="Nombre" autofocus="true">
+        </div>
+      </div>
+      <br>
+      <div class="field">
+        <div class="ui left icon input">
+          <i class="mail outline icon"></i>
+          <input type="nombre" name="nombre" placeholder="Nombre" autofocus="true">
+        </div>
+      </div>
+      <br>
+      <div class="field">
+        <div class="ui left icon input">
+          <i class="mail outline icon"></i>
+          <input type="email" name="email" placeholder="Email Address" autofocus="true">
+        </div>
+      </div>
+        <br>
+        <div class="field">
+          <div class="ui left icon input">
+            <i class="lock icon"></i>
+            <input type="password" name="password" placeholder="Password">
+          </div></div>
+          <br>
 
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="lock icon"></i>
+              <input type="password" name="password_confirmation" placeholder="Password Confirmation">
+            </div></div><br>
+
+            <div class="actions">
+              <button class=" ui large teal button" type="submit">Log in</button>
+            </div>
+            <br>
+            <p>Don't have an account? <a href="#"> Sign up </a>
+            </div>
   </body>
 </html>
