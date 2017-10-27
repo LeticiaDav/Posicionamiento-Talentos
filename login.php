ceb06939ebb29714e03984038c3544ff683649ee
Genesis Jesus <genesisdev96@outlook.com>
2017-10-26 23:50:12 -0500

    Agrego licencia
    Se agrego la licencia apache al archivo.

---
+++ login.php
@@ -0,0 +1,21 @@
+<!--
+#login.php - 1.0.1
+
+https://github.com/Genesisj96Dev/Posicionamiento-Talentos
+
+Copyright 2017 WORkTECS
+
+Desarrollado por Genesis Jesus Reyes Osnaya
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
