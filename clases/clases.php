cb082733500ecb9d231fc9fe0a48943e1b19828a
Genesis Jesus <genesisdev96@outlook.com>
2017-10-26 22:10:46 -0500

    Agrego clase usuario
    Se creo la clase usuario para poder registrar los datos obtenidos del formulario en la base de datos.

---
+++ clases/clases.php
@@ -0,0 +1,16 @@
+<?php
+
+  class usuario{
+    function registrar($datos){
+      $con = conexion_db("root", "");
+      $consulta = $con->prepare("insert into usuario(id, nombre, correo_e, contrase, universidad, perfil_profesional, foto_perfil) values(null, :nombre, :correo_e, :contrase, :universidad, :perfil_profesional, :foto_perfil)");
+      $consulta->execute(array(':nombre' => $datos[0],
+                    ':correo_e' => $datos[1],
+                    ':contrase' => $datos[2],
+                    ':universidad' => $datos[3],
+                    ':perfil_profesional' => $datos[4],
+                    ':foto_perfil' => 'img/sin foto de perfil.jpg'
+                ));
+    }
+  }
+ ?>
