f8d421f6dffb1ce26ac2e72a80cce37530793a28
Genesis Jesus <genesisdev96@outlook.com>
2017-10-26 21:01:58 -0500

    Se agregarion dos funciones
    Se creo la funcion para los datos vacios y otra para limpiar los datos.

--- funciones.php
+++ funciones.php
@@ -20,3 +20,29 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 -->
+
+<?php
+  function datos_vacios($datos){
+    $vacio = false;
+    $tamanio = count($datos);
+    for($i = 0; $i < $tamanio; $i++){
+      if(empty($datos[$i])){
+        $vacio = true;
+        break;
+      }
+    }
+    return $vacio;
+  }
+
+  function limpiar(){
+    $tamanio = count($datos);
+    for($i = 0; $i < $tamanio; $i++){
+      if($i != 2){
+        $datos[$i] = htmlspecialchars($datos[$i]);
+        $datos[$i] = trim($datos[$i]);
+        $datos[$i] = stripcslashes($datos[$i]);
+      }
+    }
+    return $datos;
+  }
+ ?>
