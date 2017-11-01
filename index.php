7c855c0c42b91ed8d3c8c17b726ee01cfe078582
Genesis Jesus <genesisdev96@outlook.com>
2017-10-31 21:20:11 -0600

    Se agrego propiedad de color para titulo
    Se agrego una propiedad de color para el titulo brand ya que el explorador chrome no reconoce el color con los estilos de boostrap,
    La etiqueta que lo usa es <span> localizada en el archivo index.php.

--- index.php
+++ index.php
@@ -52,6 +52,7 @@ verificar_sesion(); //Verificar si hay una sesion iniciada o no
     <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle mr-lg-2" id="messagesDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <i class="fa fa-fw fa-inverse fa-user-plus"></i>
             Solicitudes
           </a>
