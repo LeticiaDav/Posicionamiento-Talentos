8145dc1286ae64382bcaa80144d7117c6acb40e0
Genesis Jesus <genesisdev96@outlook.com>
2017-10-31 21:18:56 -0600

    Se agrego la barra de navegacion
    Se agrego la barra de navegacion que contendra los menus para navegar dentro de la plataforma.

--- index.php
+++ index.php
@@ -26,3 +26,63 @@ require('funciones.php');
 verificar_sesion(); //Verificar si hay una sesion iniciada o no
 //echo $_SESSION['email'];
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Posicionamiento de talentos</title>
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/semantic.min.css">
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+  <!-- Nucleo CSS de Boostrap -->
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <!-- Fuentes personalizadas -->
+  <link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <!-- Estilos personalizados por Start Bootstrap - SB Admin v4.0.0-beta -->
+  <link href="css/styles.css" rel="stylesheet">
+</head>
+
+<body >
+  <!-- Navigation-->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <a class="navbar-brand mb-0 h1" href="#"><span class="white">Posicionamiento de Talentos</span></a>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown">
+            <i class="fa fa-fw fa-inverse fa-user-plus"></i>
+            Solicitudes
+          </a>
+          <div class="dropdown-menu" aria-labelledby="SolicitudesDropdown">
+            <h6 class="dropdown-header">Solicitudes De Amistad:</h6>
+          </div>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle mr-lg-2" id="messagesDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fa fa-fw fa-inverse fa-bell"></i>
+            Notificaciones
+          </a>
+          <div class="dropdown-menu" aria-labelledby="messagesDropdown">
+            <h6 class="dropdown-header">Notificaciones Nuevas:</h6>
+          </div>
+        </li>
+
+        <li class="nav-item">
+          <form class="form-inline my-2 my-lg-0 mr-lg-2">
+            <div class="input-group">
+              <input class="form-control" type="text" placeholder="Search for...">
+            </div>
+          </form>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <!-- Nucleo javascript de Boostrap -->
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="js/popper/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
