2b232ca08858da5c5692cc1b045a224292f2d3b6
Genesis Jesus <genesisdev96@outlook.com>
2017-11-03 12:49:20 -0600

    Se agrego la seccion de comentarios de la publicacion

--- index.php
+++ index.php
@@ -128,6 +128,47 @@ verificar_sesion(); //Verificar si hay una sesion iniciada o no
     </div>
   </section>
 
+  <section>
+    <article class="noticia">
+      <div class="card">
+        <div class="card-header" id="cabecera-card">
+          <table>
+            <tr>
+              <td><a href="#"><img src="img/no-profile.png" class="imagen-inicial-perfil" alt=""></a></td>
+              <td><a href="#" class="nombre-usuario">Usuario 1</a></td>
+            </tr>
+          </table>
+        </div>
+        <div class="card-body-noticia">
+          <p class="card-text-noticia">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+          <div class="stats">
+            <a href="#" class="stat-item"><i class="fa fa-thumbs-up icon"></i>1</a>
+            <a href="#" class="stat-item"><i class="fa fa-comments-o icon"></i>1</a>
+          </div>
+        </div>
+        <div class="card-footer" id="pie-card-comentarios">
+          <form class="comentario" action="<?php echo $_SERVER[PHP_SELF]; ?>" method="post">
+            <input class="form-control add-comment-input" placeholder="Add a comment..." type="text">
+          </form>
+          <!-- <ul class="comments-list">
+            <li class="comment">
+              <a class="pull-left" href="#">
+                <img class="avatar" src="img/no-profile.png" alt="avatar">
+              </a>
+              <div class="comment-body">
+                <div class="comment-heading">
+                  <h4 class="comment-user-name"><a href="#">Usuario 2</a></h4>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </div>
+            </li>
+          </ul> -->
+        </div>
+      </div>
+    </article>
+
+  </section>
+
   <!-- Nucleo javascript de Boostrap -->
   <script src="js/jquery-3.2.1.min.js"></script>
   <script src="js/popper/popper.min.js"></script>
