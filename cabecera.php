3cb30a641806e470804afee526ee8961f613a457
Genesis Jesus <genesisdev96@outlook.com>
2017-11-07 11:53:50 -0600

    Se agrego la version final a usar del css de bootstrap

--- cabecera.php
+++ cabecera.php
@@ -29,7 +29,8 @@ limitations under the License.
   <title>Posicionamiento de talentos</title>
   <script src="js/jquery-3.2.1.min.js"></script>
   <!-- Nucleo CSS de Boostrap -->
-  <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"> -->
   <!-- Fuentes personalizadas -->
   <link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
   <!-- Estilos personalizados -->
