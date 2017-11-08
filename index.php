413bfe8b1c3babacf24d9daf4cc5d2fee02fecc0
Genesis Jesus <genesisdev96@outlook.com>
2017-11-07 18:38:37 -0600

    Se agrego la barra de navegacion
    Se agrego la barra de navegacion de la pagina de inicio

--- index.php
+++ index.php
@@ -19,3 +19,37 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 -->
+
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Posicionamiento de talentos</title>
+  <!-- Nucleo CSS de Boostrap -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"> -->
+</head>
+
+<body>
+  <nav class="navbar navbar-dark  navbar-expand-md fixed-top" style="background-color: #1B1C1D;">
+    <a class="navbar-brand" href="#">
+      <img src="img/brand.png" width="30" height="30" class="d-inline-block align-top" alt="">
+      Posicionamiento De Talentos
+    </a>
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+  </ul>
+</nav>
+</body>
+</html>
